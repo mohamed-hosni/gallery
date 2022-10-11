135b38ebaf847411f143f2f36585f170d95a39a4
Abdalrhman Hussin <abdalrhmanhussin@gmail.com>
2022-10-11 16:59:28 +0200

    make function find this query

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -5,16 +5,24 @@ class User{
 public static function find_all_users(){
 global  $db;
 
-$result_set= $db->query("select * from users");
+// $result_set= $db->query("select * from users");
+$result_set=self::find_this_query(("select * from users"));
 return $result_set;
     }
 
     public static function find_user_by_id($id){
         global  $db;
         
-        $result_set= $db->query("select * from users where `id`=$id limit 1");
-        $Result=mysqli_fetch_array($result_set);
-        return $Result;
+        //$result_set= $db->query("select * from users where `id`=$id limit 1");
+        $Result=self::find_this_query("select * from users where id= $id");
+        
+        $result_set=mysqli_fetch_array($Result);
+        return $result_set;
+            }
+            public static function find_this_query($sql){
+                global $db;
+                 $result_set=$db->query($sql);
+return $result_set;
             }
 
 
